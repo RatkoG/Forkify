0006100a44935ef18cef96f27b86b33f38fc9a72
ratko <ratko.gjurichanin@gmail.com>
2019-11-24 13:57:52 -0600

    Getting the value from search input

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import Search from './models/Search';
+import * as searchView from './view/searchView';
+import {
+  elements
+} from './view/base';
 /**GLOBAL STATE OF THE APP
  * - Search object
  * - Current recipe object
@@ -11,7 +15,9 @@ import Search from './models/Search';
 const state = {};
 const controlSearch = async () => {
   // * 1) Get query from view
-  const query = 'pizza'; //TODO
+  const query = searchView.getInput(); //TODO
+  console.log(query);
+
 
   if (query) {
     // * 2) New search object and add to state
@@ -28,7 +34,7 @@ const controlSearch = async () => {
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 })
---
+++ src/js/view/searchView.js
@@ -0,0 +1,5 @@
+import {
+  elements
+} from './base';
+// This is implicit return. we dont need to write return e
+export const getInput = () => elements.searchInput.value;
