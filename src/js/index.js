965d5c0759f0d7b5a1facfa5a945d79ab8f822c5
ratko <ratko.gjurichanin@gmail.com>
2019-11-24 08:15:37 -0600

    This is 3 way where you can import everything.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import string from './models/Search';
-import {
-  add as a,
-  multiply as b,
-  ID
-} from './view/searchView';
+// import {
+//   add as a,
+//   multiply as b,
+//   ID
+// } from './view/searchView';
+// console.log(`Using imported functions! ${a(ID,2)} and ${b(3,5)}. ${string}`);
 
-console.log(`Using imported functions! ${a(ID,2)} and ${b(3,5)}. ${string}`);
+import * as searchView from './view/searchView';
+console.log(`Using imported functions! ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3,5)}. ${string}`);
