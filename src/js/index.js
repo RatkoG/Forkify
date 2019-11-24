f1e62ac31983b97d613faf8b867acc0ecaffd4b3
ratko <ratko.gjurichanin@gmail.com>
2019-11-24 08:26:46 -0600

    3 way commented

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import string from './models/Search';
+
+// * This way you can import multiple things from one module
 // import {
 //   add as a,
 //   multiply as b,
@@ -6,5 +8,6 @@ import string from './models/Search';
 // } from './view/searchView';
 // console.log(`Using imported functions! ${a(ID,2)} and ${b(3,5)}. ${string}`);
 
+// * This way you can import everything
 import * as searchView from './view/searchView';
 console.log(`Using imported functions! ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3,5)}. ${string}`);
