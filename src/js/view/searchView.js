aa3f2620dc3e1d35bb143057aa0e5dec5d8edfa7
ratko <ratko.gjurichanin@gmail.com>
2019-11-26 11:07:52 -0600

    Display title with 17 or less characters
    We have created that function. Its pretty cool

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,30 +1,42 @@
-import {
-  elements
-} from './base';
+import { elements } from "./base";
 
 // This is implicit return. we dont need to write return e
 export const getInput = () => elements.searchInput.value;
 export const clearInput = () => {
-  elements.searchInput.value = '';
-}
+  elements.searchInput.value = "";
+};
 export const clearResults = () => {
-  elements.searchResList.innerHTML = '';
-}
+  elements.searchResList.innerHTML = "";
+};
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, curr) => {
+      if (acc + curr.length <= limit) {
+        newTitle.push(curr);
+      }
+      return acc + curr.length;
+    }, 0);
+    //  return the result
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
-  const markup =
-    `<li>
+  const markup = `<li>
           <a class="results__link" href="#${recipe.recipe_id}">
               <figure class="results__fig">
                 <img src="${recipe.image_url}" alt="${recipe.title}">
               </figure>
             <div class="results__data">
-            <h4 class="results__name">${recipe.title} ...</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)} ...</h4>
             <p class="results__author">${recipe.publisher}</p>
           </div>
         </a>
 </li>`;
-  elements.searchResList.insertAdjacentHTML('beforeend', markup);
-}
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
 export const renderResults = recipes => {
   recipes.forEach(renderRecipe);
 };
