0d9dba720e1c3fd45b1c2f3683fedbea03aca384
ratko <ratko.gjurichanin@gmail.com>
2019-11-27 19:18:16 -0600

    UniqID Add and delete items

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
-import Recipe from './models/Recipe'
+import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './view/searchView';
 import * as recipeView from './view/recipeView';
 import {
@@ -136,3 +137,5 @@ elements.recipe.addEventListener('click', e => {
   console.log(state.recipe);
 
 })
+
+window.l = new List();
